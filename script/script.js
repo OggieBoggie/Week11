0c192ccd2272a58040bc25a1b18bb13e7b685326
OggieBoggie <tommy.nguyen.vanc@gmail.com>
2022-11-16 13:22:16 -0800

    Circle area function

--- script/script.js
+++ script/script.js
@@ -4,10 +4,21 @@
 // }
 
 
-const greet = function(user, time = "day") {
-    console.log (`Good ${time} ${user}!`);
-}
-let time = prompt("What time is it")
-let user = prompt("Who are you?");
-greet(user, time);
+// const greet = function(user, time = "day") {
+//     console.log (`Good ${time} ${user}!`);
+// }
 
+// let time = prompt("What time is it")
+// let user = prompt("Who are you?");
+// greet(user, time);
+
+
+
+const calcArea = function(radius) {
+    let result = Math.pi * radius * radius;
+    return result;
+}
+
+let r = prompt("Enter the radius of circle");
+let calculatedArea = calcArea(r).toFixed(2);
+alert(`The area of a circle with radius ${r} is ${calculatedArea}`);
