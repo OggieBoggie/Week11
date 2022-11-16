00a1784ff21aac7e5b94c63953b10e6c54810639
OggieBoggie <tommy.nguyen.vanc@gmail.com>
2022-11-16 14:05:44 -0800

    Change list type

--- script/script.js
+++ script/script.js
@@ -13,16 +13,38 @@
 // greet(user, time);
 
 
-function calcArea(radius) {
-    let result = Math.PI * radius * radius;
-    return result;
+// function calcArea(radius) {
+//     let result = Math.PI * radius * radius;
+//     return result;
+// }
+
+// let r = prompt("Enter the radius of circle");
+// let calculatedArea = calcArea(r).toFixed(2);
+// alert(`The area of a circle with radius ${r} is ${calculatedArea}`);
+
+// let radiusParagraph = document.querySelector('#radius');
+// radiusParagraph.textContent += r;
+// let areaParagraph = document.querySelector('#result');
+// areaParagraph.textContent += calculatedArea;
+
+let shoppingItems = ["cheese" , "chicken" , "chair"];
+
+const shoppingListElement = document.querySelector(".shopping")
+function populateList(arr)
+{
+    for (let item of arr) {
+    let listItem = document.createElement("li");
+    listItem.textcontent = item;
+    shoppingListElement.appendChild(listItem);
+    }
 }
 
-let r = prompt("Enter the radius of circle");
-let calculatedArea = calcArea(r).toFixed(2);
-alert(`The area of a circle with radius ${r} is ${calculatedArea}`);
+populateList(shoppingItems);
 
-let radiusParagraph = document.querySelector('#radius');
-radiusParagraph.textContent += r;
-let areaParagraph = document.querySelector('#result');
-areaParagraph.textContent += calculatedArea;
+function changeListStyle()
+{
+    shoppingListElement.classList.remove("circleList");
+    shoppingListElement.classList.add("squareList");
+}
+
+changeListStyle()
